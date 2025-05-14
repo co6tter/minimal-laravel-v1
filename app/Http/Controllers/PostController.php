80e0762e040d6a386603b3bad372f4c7eeeaa028
co6tter <co6tter@gmail.com>
2025-05-14 18:48:29 +0900

    chore: add mysql sample

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,22 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    private  $posts = [
-        'Title A',
-        'Title B',
-        'Title C'
-    ];
-
     public function index()
     {
-        return view('index')->with(['posts' => $this->posts]);
+        $posts = Post::latest()->get();
+
+        return view('index')->with(['posts' => $posts]);
     }
 
-    public function show($id)
+    public function show(Post $post)
     {
-        return view('posts.show')->with(['post' => $this->posts[$id]]);
+        return view('posts.show')->with(['post' => $post]);
     }
 }
